fc105acbd5a95680ee3b058f6768531aaff87864
Aman Islam <amanislam019@gmail.com>
2024-11-05 23:14:56 +0500

    basic template

---
+++ countries/src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Hero from "./components/hero/Hero"
+import Practice from "./components/practice/Practice"
+import Stat from "./components/Statistics/Stat";
+
+
+export default function Home() {
+  return (
+    <main>
+      {/* <Hero/> */}
+      <Practice/>
+      <Stat/>
+   
+      
+    
+
+   
+    </main>
+  );
+}
